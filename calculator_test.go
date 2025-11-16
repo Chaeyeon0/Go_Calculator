8a530fc2944501d82701b490b869503b152783b4
qorcodusl <qorcodus03@naver.com>
2025-11-17 00:36:27 +0900

    Test: 쉼표와 콜론 분리에 대한 테스트 작성

---
+++ calculator_test.go
@@ -0,0 +1,13 @@
+package gocalculator
+
+import "testing"
+
+func TestAdd_Basic(t *testing.T) {
+	result, err := Add("1,2,3")
+	if err != nil {
+		t.Fatalf("에러: %v", err)
+	}
+	if result != 6 {
+		t.Errorf("결과가 %d입니다. 기대값은 6이에요.", result)
+	}
+}
