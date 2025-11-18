0991a2de6da5da46d635614521779af9a0c111a2
qorcodusl <qorcodus03@naver.com>
2025-11-19 00:36:25 +0900

    Feat: 계산기 기능 로직 개선 및 테스트
    앞서 커밋한 파일들에 대해 책임 분리 후 로직을 수정하고 테스트 (덧셈)

--- caculator.go
+++ caculator.go
@@ -1,46 +1,9 @@
 package gocalculator
 
-import (
-	"errors"
-	"regexp"
-	"strconv"
-	"strings"
-)
-
-func Add(input string) (int, error) {
-	if strings.TrimSpace(input) == "" {
-		return 0, nil
+func Sum(nums []int) int {
+	total := 0
+	for _, n := range nums {
+		total += n
 	}
-
-	delimiters := ",|:" // 기본 구분자
-	numbers := input
-
-	// 커스텀 구분자 형식 검사
-	if strings.HasPrefix(input, "//") {
-		re := regexp.MustCompile(`^//(.)\n(.*)`)
-		matches := re.FindStringSubmatch(input)
-		if len(matches) == 3 {
-			delimiters = regexp.QuoteMeta(matches[1])
-			numbers = matches[2]
-		} else {
-			return 0, errors.New("잘못된 구분자 형식입니다")
-		}
-	}
-
-	re := regexp.MustCompile(delimiters)
-	tokens := re.Split(numbers, -1)
-
-	sum := 0
-	for _, t := range tokens {
-		num, err := strconv.Atoi(strings.TrimSpace(t))
-		if err != nil {
-			return 0, err
-		}
-		if num < 0 {
-			return 0, errors.New("음수는 입력할 수 없습니다")
-		}
-		sum += num
-	}
-
-	return sum, nil
+	return total
 }
--- calculator_test.go
+++ calculator_test.go
@@ -2,29 +2,9 @@ package gocalculator
 
 import "testing"
 
-func TestAdd_Basic(t *testing.T) {
-	result, err := Add("1,2,3")
-	if err != nil {
-		t.Fatalf("에러: %v", err)
-	}
+func TestSum(t *testing.T) {
+	result := Sum([]int{1, 2, 3})
 	if result != 6 {
-		t.Errorf("결과가 %d입니다. 기대값은 6이에요.", result)
-	}
-}
-
-func TestAdd_CustomDelimiter(t *testing.T) {
-	result, err := Add("//;\n1;2;3")
-	if err != nil {
-		t.Fatalf("에러 발생: %v", err)
-	}
-	if result != 6 {
-		t.Errorf("결과가 %d입니다. 기대값은 6이에요.", result)
-	}
-}
-
-func TestAdd_NegativeNumber(t *testing.T) {
-	_, err := Add("1,-2,3")
-	if err == nil {
-		t.Errorf("음수가 들어가면 에러가 발생합니다.")
+		t.Errorf("결과가 %d 입니다. 기대값은 6이에요.", result)
 	}
 }
